7ec5ed840c5d53eefbabefbbb0f7a06136440f57
azhar910 <azhar@taplane.com>
2023-06-17 14:48:55 -0400

    refactor

--- onlinelearning/onlinelearningapp/models.py
+++ onlinelearning/onlinelearningapp/models.py
@@ -4,88 +4,117 @@ from django.db import models
 
 class Membership(models.Model):
     name = models.CharField(max_length=30)
-
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    currency = models.CharField(max_length=3)
 
     def __str__(self):
-        return self.name
+        return f"Name:{self.name},Price:{self.price},Currency:{self.currency}"
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"RoleName:{self.name}"
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    role = models.CharField(max_length=30)
+    role = models.ForeignKey(Role)
 
-    membership = models.ForeignKey(Membership, on_delete=models.SET_NULL, null=True, blank=True)
+    membership = models.ForeignKey(Membership, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return self.user.username
+        return f"Name:{self.user.username},Role:{self.role.name},Membership:{self.membership.name}"
 
 
-# TODO: add a field in Course for gold bronze silver
-# TODO: add the section logic by creating a new model
-# TODO: map the section to coursecontent model
 '''
 Course:
 cid
 
 Section:
-sid1 cid order
-sid2 cid order
-sid3 cid order
+sid1 cid order1
+sid2 cid order2
+sid3 cid order3
 
 CourseContent:
-ccid1 sid1 link order type
-ccid2 sid1 link order type
-ccid3 sid1 link order type
+ccid1 sid1 link order1 type
+ccid2 sid1 link order2 type
+ccid3 sid1 link order3 type
 '''
 
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
-
     description = models.TextField()
-
     instructor = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    published = models.BooleanField()
+    published = models.BooleanField(default=False)
+    membership_level = models.ForeignKey(Membership, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return self.name
+        return f"CourseName:{self.name},Published:{self.published},Instructor:{self.instructor.name},Membership Level:{self.membership_level.name}"
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    order = models.IntegerField()
+
+    class Meta:
+        unique_together = ('course', 'order',)
+
+    def __str__(self):
+        return f"CourseName:{self.course.name},SectionName:{self.name},Instructor:{self.instructor.name},Order:{self.order}"
+
+
+class CourseContent(models.Model):
+    CONTENT_TYPES = (
+        ('pdf', 'PDF'),
+        ('video', 'VIDEO'),
+        ('txt', 'TEXT'),
+    )
+
+    def course_file_path(self, filename):
+        # courses/courseId/SectionID/file.pdf
+        return f'courses/{self.section.course.id}/section_{self.section.id}/{filename}'
+
+    name = models.CharField(max_length=100)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    filepath = models.FileField(upload_to=course_file_path)
+    order = models.IntegerField()
+    content_type = models.CharField(max_length=5, choices=CONTENT_TYPES)
+
+    class Meta:
+        unique_together = ('section', 'order',)
+
+    def __str__(self):
+        return f'{self.section.course.name} - Section {self.section.order} - {self.content_type} {self.order}'
 
 
 class Enrollment(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE)
-
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
     date_enrolled = models.DateTimeField(auto_now_add=True)
-
-    progress = models.IntegerField()
 
     def __str__(self):
         return f'{self.student.username} - {self.course.name}'
 
 
-# TODO: add a model for CourseProgress which keeps tracks of each individual course content completed or not
 class CourseProgress(models.Model):
-    student = models.ForeignKey(User, on_delete=models.CASCADE)
-    # TODO: map it to couse content
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    course_content = models.ForeignKey(CourseContent, on_delete=models.CASCADE)
     status = models.BooleanField()
 
     def __str__(self):
-        return f'{self.student.username} - {self.course.name}'
+        return f'{self.enrollment.student.username} - {self.course_content.section.course.name} - {self.course_content.content_type} {self.course_content.order}'
 
 
 class Attendance(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE)
-
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
     date = models.DateField()
-
-    status = models.BooleanField()
 
     def __str__(self):
         return f'{self.student.username} - {self.course.name} - {self.date}'
@@ -93,10 +122,9 @@ class Attendance(models.Model):
 
 class Certificate(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE)
-
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
     issue_date = models.DateTimeField(auto_now_add=True)
+    filepath = models.CharField(max_length=500)
 
     def __str__(self):
         return f'{self.student.username} - {self.course.name}'
@@ -104,15 +132,7 @@ class Certificate(models.Model):
 
 class Payment(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True)
-
     membership = models.ForeignKey(Membership, on_delete=models.CASCADE, null=True, blank=True)
-
-    amount = models.DecimalField(max_digits=5, decimal_places=2)
-
-    currency = models.CharField(max_length=3)
-
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
