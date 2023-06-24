712d2b08e8f34943f2c9d9b8275721def14deea9
azhar910 <azhar@taplane.com>
2023-06-24 05:25:35 -0400

    base strucute

--- onlinelearning/onlinelearning/urls.py
+++ onlinelearning/onlinelearning/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from onlinelearningapp.views import register
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register, name='register'),
 ]
--- onlinelearning/onlinelearningapp/views.py
+++ onlinelearning/onlinelearningapp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import Role, UserProfile
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        role = request.POST['role']
+
+        # Create a new User object
+        user = User.objects.create_user(username=username, email=email, password=password)
+
+        # Get the Role object based on the selected role
+        role_obj = Role.objects.get(name=role)
+
+        # Create a UserProfile object
+        UserProfile.objects.create(user=user, role=role_obj)
+
+        return redirect('home')  # Redirect to the home page after successful registration
+
+    return render(request, 'onlinelearning/register.html')
